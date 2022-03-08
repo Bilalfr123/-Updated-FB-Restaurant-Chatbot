839d0ebf9cdbf842283586a0e0c3251edbe8e49d
unknown <darklight1062@gmail.com>
2022-03-07 23:18:15 -0800

    updates

--- src/public/js/survey.js
+++ src/public/js/survey.js
@@ -1,4 +1,3 @@
-
 (function(d, s, id){
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) {return;}
@@ -6,31 +5,45 @@
     js.src = "//connect.facebook.net/en_US/messenger.Extensions.js";
     fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'Messenger'));
-
-
   window.extAsyncInit = function() {
     // the Messenger Extensions JS SDK is done loading 
 
-    //get user PSID
-    MessengerExtensions.getContext(facebookAppId, 
+    //get psid
+    MessengerExtensions.getContext( facebookAppId, 
   function success(thread_context){
-      let userPSID = thread_context.psid
-      document.getElementById("psid").value =  userPSID 
+    let userPSID = thread_context.psid;
+    document.getElementById("psid").value = userPSID;
     // success
   },
   function error(err){
     // error
   }
 );
+
 document.getElementById("submitBtn").addEventListener('click', function(){
     console.log('working')
     MessengerExtensions.requestCloseBrowser(function success() {
         // webview closed
+        //send request to nodejs servere to store data to google sheets
+        let dataBody = {
+            psid :   document.getElementById("psid").value,
+            name :   document.getElementById("name").value,
+            country :   document.getElementById("country").value,
+            email :   document.getElementById("email").value,
+            phone :   document.getElementById("phone").value
+        }
+        fetch(`${window.location.origin}/post-survey`, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(dataBody)
+    }).then(res => res.json())
+      .then(res => console.log(res));
+    
       }, function error(err) {
-          console.log(err)
         // an error occurred
       });
-
-})
-
-  };
+    });
+};
