094ac6a79eeadef2a1d230cc32d79cf9aee22d7c
unknown <darklight1062@gmail.com>
2022-03-07 23:01:28 -0800

    updates

--- src/public/js/survey.js
+++ src/public/js/survey.js
@@ -27,23 +27,23 @@ document.getElementById("submitBtn").addEventListener('click', function(){
 //on close webview
 MessengerExtensions.requestCloseBrowser(function success() {
     // webview closed
-    //send request to nodejs servere to store data to google sheets
-    let dataBody = {
-        psid :   document.getElementById("psid").value,
-        name :   document.getElementById("name").value,
-        country :   document.getElementById("country").value,
-        email :   document.getElementById("email").value,
-        phone :   document.getElementById("phone").value
-    }
-    fetch(`${window.location.origin}/post-survey`, {
-  method: 'POST',
-  headers: {
-    'Accept': 'application/json, text/plain, */*',
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify(dataBody)
-}).then(res => res.json())
-  .then(res => console.log(res));
+//     //send request to nodejs servere to store data to google sheets
+//     let dataBody = {
+//         psid :   document.getElementById("psid").value,
+//         name :   document.getElementById("name").value,
+//         country :   document.getElementById("country").value,
+//         email :   document.getElementById("email").value,
+//         phone :   document.getElementById("phone").value
+//     }
+//     fetch(`${window.location.origin}/post-survey`, {
+//   method: 'POST',
+//   headers: {
+//     'Accept': 'application/json, text/plain, */*',
+//     'Content-Type': 'application/json'
+//   },
+//   body: JSON.stringify(dataBody)
+// }).then(res => res.json())
+//   .then(res => console.log(res));
 
   }, function error(err) {
     // an error occurred
