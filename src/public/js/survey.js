0a599ccf4e727ec4b4383f394921406d4be10a8b
unknown <darklight1062@gmail.com>
2022-03-08 06:06:56 -0800

    updates

--- src/public/js/survey.js
+++ src/public/js/survey.js
@@ -26,25 +26,25 @@ window.extAsyncInit = function () {
 document.getElementById('submitBtn').addEventListener('click',function ()
 {console.log("hi")
 MessengerExtensions.requestCloseBrowser(function success() {
-  window.close()
+  window.top.close()
   //webview closed
-  let dataBody = {
-      // psid: document.getElementById("psid").value,
-      name: document.getElementById("name").value,
-      country: document.getElementById("country").value,
-      email: document.getElementById("email").value,
-    phonenumber: document.getElementById("phone").value,
+  // let dataBody = {
+  //     // psid: document.getElementById("psid").value,
+  //     name: document.getElementById("name").value,
+  //     country: document.getElementById("country").value,
+  //     email: document.getElementById("email").value,
+  //   phonenumber: document.getElementById("phone").value,
   
-  }
-  fetch(`${window.location.origin}/post-survey`, {
-      method: 'POST',
-      headers: {
-          'Accept': 'application/json, text/plain, */*',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(dataBody)
-      }).then(res => res.json())
-      .then(res => console.log(res));
+  // }
+  // fetch(`${window.location.origin}/post-survey`, {
+  //     method: 'POST',
+  //     headers: {
+  //         'Accept': 'application/json, text/plain, */*',
+  //         'Content-Type': 'application/json'
+  //       },
+  //       body: JSON.stringify(dataBody)
+  //     }).then(res => res.json())
+  //     .then(res => console.log(res));
     }, function error(err) {
       // an error occurred
       console.log(err)
